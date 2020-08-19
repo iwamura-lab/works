ae9b9e396b1128202b256a03647ccce6fb6cf978
Taiki Iwamura <takki.0206@gmail.com>
2020-08-19 17:06:42 +0900

    Add function

--- calc_invariant.py
+++ calc_invariant.py
@@ -1,3 +1,6 @@
+'''
+Practice of python coding and software development by using github
+'''
 # set python interpreter(2 or 3 ?)
 # !/usr/bin/python3
 # -*- coding: UTF-8 -*-
@@ -9,3 +12,16 @@ import numpy as np
 # import modules related to materialsProject
 import pymatgen as mg
 
+def calc_sph_harm(sph_indices, phi, theta):
+    """
+    Docstring:
+    calc_sph_harm(sph_indices, phi, theta)
+
+    Calculate spherical harmonics under given arguments
+    (sph_indices: [l, m], phi: azimuthal angle, theta: polar angle).
+    Since the default definition of quantum numbers and angles is not natural
+    to physicists, there was urgent necessity to modify.
+    """
+
+    sph_indices = sph_indices.reverse()
+    return sp.sph_harm(sph_indices[0], sph_indices[1], phi, theta)
