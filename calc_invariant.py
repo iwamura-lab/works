8ae1b975c6269c6a0926b629bb4e31bf5c6b8524
Taiki Iwamura <takki.0206@gmail.com>
2020-08-19 15:46:31 +0900

    import modules

---
+++ calc_invariant.py
@@ -0,0 +1,11 @@
+# set python interpreter(2 or 3 ?)
+# !/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+# import modules including mathematical functions
+import scipy.special as sp
+import numpy as np
+
+# import modules related to materialsProject
+import pymatgen as mg
+
