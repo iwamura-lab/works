a9fc08d802abe6b13e71bae5a4eb4759654d3fcf
Taiki Iwamura <takki.0206@gmail.com>
2020-09-22 16:06:47 +0900

    Create a new file and change existing file

---
+++ projmat/invariants.py
@@ -0,0 +1,13 @@
+'''
+Program to compute rotational invariants of SO3 group by using
+projection operator method
+This deals with only spherical harmonics, but may be improved to use
+other functions as basis functions in the near future
+'''
+
+# !/usr/bin/env/python
+# -*- coding: UTF-8 -*-
+
+# Import modules to calculate
+import numpy as np
+""
