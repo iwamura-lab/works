95f613d5287a85b4054da77acf67c1754ce23404
Taiki Iwamura <takki.0206@gmail.com>
2020-10-05 17:18:45 +0900

    Add function to save

--- projmat/order_parameter.py
+++ projmat/order_parameter.py
@@ -5,6 +5,9 @@ Program to calculate order parameters when selecting spherical harmonics as basi
 # !/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
+# import standard modules
+import pickle
+
 # import modules including mathematical functions
 import scipy.special as sp
 import numpy as np
@@ -12,13 +15,49 @@ import numpy as np
 # import modules related to materialsProject
 import pymatgen as mg
 
-def calc_sph_harm(sph_indices, phi, theta):
-    """
-    Docstring:
-    calc_sph_harm(sph_indices, phi, theta)
+def calc_order_parameter(_structure, each_site, quantum, cut_off, params):
+    """Calculate order parameter
 
-    Calculate spherical harmonics under given arguments
-    (sph_indices: [l, m], phi: azimuthal angle, theta: polar angle).
+    Args:
+        _structure (Structure): crystal structure in POSCAR format
+        each_site (Sites): the position of an atom
+        _neighbors (list): neighboring atoms of each_site
+        quantum (list): [l, m]
+        cut_off (float): cut off radius
+        params (dict): {"center": float, "height": float}
+
+    Returns:
+        float: order parameter
+    """
+    res = 0.0
+    _neighbors = _structure.get_neighbors(each_site, cut_off)
+    for neighbor in _neighbors:
+        # transform cartesian coordinates into polar coordinates
+        vec = neighbor.coords - each_site.coords
+        r = np.linalg.norm(vec)
+        theta = np.arccos(vec[2]/r)
+        phi = np.arccos(vec[0]/(r*np.sin(theta)))
+        if vec[1]/np.sin(theta) < 0:
+            phi = -phi + 2 * np.pi
+        # calculate radial function
+        gauss = np.exp(- params["height"] * ((r - params["center"])**2))
+        func_cut = 0.5 * (np.cos(np.pi * r/cut_off)+1)
+        radial = gauss * func_cut
+        res += radial * calc_sph_harm(quantum, phi, theta)
+    return res.conjugate()
+
+
+def calc_sph_harm(sph_indices, phi, theta):
+    """Calculate spherical harmonics under given arguments
+
+    Args:
+        sph_indices (list): [l, m]
+        phi (float): azimuthal angle
+        theta (float): polar angle
+
+    Returns:
+        float: spherical harmonics
+
     Since the default definition of quantum numbers and angles is not natural
     to physicists, there was urgent necessity to modify.
     """
@@ -26,57 +65,42 @@ def calc_sph_harm(sph_indices, phi, theta):
     return sp.sph_harm(sph_indices[1], sph_indices[0], phi, theta)
 
 
-def calc_order_parameter(poscar, l, m, cut_off, params):
-    """
-    Docstring:
-    calc_order_parameter(poscar, l, m, cut_off, params)
+def calc_opl(poscar, lmax, cut_off, params):
+    """Calculate multi list of order parameters in using spherical harmonics as basis functions
 
-    Calculate order parameters in using spherical harmonics as basis functions.
-    -----------------------------------------------------------------------------
-    Input
-        poscar (POSCAR) : the path of POSCAR file(information of atomic positions)
-        l      (int)    : azimuthal quantum number
-        m      (int)    : magnetic quantum number
-        cut_off(float)  : cut off radius
-        params (dict)   : parameters of radial function
-                          {center: float,  height: float}
-    -----------------------------------------------------------------------------
+    Args:
+        poscar (POSCAR): the path of POSCAR file(information of atomic positions)
+        lmax (int): maximum azimuthal quantum number
+        cut_off (float): cut off radius
+        params (dict): parameters of radial function {center: float, height: float}
+
+    Returns:
+        list: order parameter centered at atoms in unit cell
     """
+
     # get information of atomic positions by reading POSCAR file
     _structure = mg.Structure.from_file(poscar)
     atom_sites = _structure.sites
-    # change data structure of quantum numbers
-    quantum = [l, m]
-    results = []
+    # prepare data structure
+    res = []
 
     # get information of neighboring atoms and calculate order parameters
     for each_site in atom_sites:
-        # prepare data structures
-        a = 0
-        _neighbors = _structure.get_neighbors(each_site, cut_off)
-        for neighbor in _neighbors:
-            # transform cartesian coordinates into polar coordinates
-            vec = neighbor.coords - each_site.coords
-            r = np.linalg.norm(vec)
-            theta = np.arccos(vec[2]/r)
-            phi = np.arccos(vec[0]/(r*np.sin(theta)))
-            if vec[1]/np.sin(theta) < 0:
-                phi = -phi + 2 * np.pi
-            # calculate radial function
-            gauss = np.exp(- params["height"] * ((r - params["center"])**2))
-            func_cut = 0.5 * (np.cos(np.pi * r/cut_off)+1)
-            radial = gauss * func_cut
-            a += radial * calc_sph_harm(quantum, phi, theta)
-        # make the list of order parameters
-        results.append(a)
-    return np.conj(results)
+        # prepare data structure
+        al = []
+        for l in range(lmax+1):
+            a = []
+            for m in range(-l, l+1):
+                a.append(calc_order_parameter(_structure, each_site, [l, m], cut_off, params))
+            # make the list of order parameters
+            al.append(a)
+        res.append(al)
+    return res
 
 if __name__ == "__main__":
     PATH = "dataset/O1Zn1_CONTCAR"
-    n1 = int(input("Enter the azimuthal quantum number.:"))
-    n2 = int(input("Enter the magnetic quantum number.:"))
+    lmax = int(input("Enter the maximum of azimuthal quantum number.:"))
     cr = float(input("Enter the cut-off radius.:"))
-    rpar = {"center": 0, "height": 0}
-    rpar["center"] = float(input("Enter the central position of radial function.:"))
-    rpar["height"] = float(input("Enter the height of radial function.:"))
-    print(calc_order_parameter(PATH, n1, n2, cr, rpar))
+    rpar = {"center": 0.0, "height": 1.0}
+    opl = calc_opl(PATH, lmax, cr, rpar)
+    pickle.dump(opl, open("results/opl.dump", "wb"))
