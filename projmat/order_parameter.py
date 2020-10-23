373afe2b1c89e11b5914031469c2c4a74ad380bc
Taiki Iwamura <taiki-engineering@outlook.jp>
2020-10-23 10:38:41 +0900

    Complete alpha

--- projmat/order_parameter.py
+++ projmat/order_parameter.py
@@ -64,16 +64,16 @@ def gaussian(x, mu, sigma):
         float: result of calculation
     """
 
-    return np.exp(-(x - mu)**2/(2*sigma**2))
+    return np.exp(-(x - mu)**2/(2 * sigma**2))
 
-def a_int_R(r, theta, phi, qnum, cut_off, mu):
+def a_int_R(r, theta, phi, nlm, cut_off, mu):
     """Return real part of integrand to use scipy.integrate.tplquad
 
     Args:
         r (float): radius
         theta (float): polar angle
         phi (float): azimuthal angle
-        qnum (tuple): tuple of quantum numbers
+        nlm (tuple): tuple of center and tuple of quantum numbers (l, m)
         cut_off (float): cut_off radius
         mu (list): cartesian coordinates of neighboring atom
 
@@ -83,8 +83,8 @@ def a_int_R(r, theta, phi, qnum, cut_off, mu):
 
     # set a value to sigma
     # sigma = 8.5 * 10**(-3)
-    sigma = 0.1
-    # sigma = 1
+    # sigma = 0.1
+    sigma = 1
     # calculate cartesian coordinates from polar coordinates
     x = r * np.cos(phi) * np.sin(theta)
     y = r * np.sin(phi) * np.sin(theta)
@@ -95,20 +95,20 @@ def a_int_R(r, theta, phi, qnum, cut_off, mu):
     fy = gaussian(y, mu[1], sigma)
     fz = gaussian(z, mu[2], sigma)
     # calculate radial function and spherical harmonics
-    gauss = np.exp(- (r-3)**2)
+    gauss = np.exp(- (r-nlm[0])**2)
     func_cut = 0.5 * (np.cos(np.pi * r/cut_off)+1)
     radial = gauss * func_cut
-    sph = sph_harm_R(phi, theta, qnum[0], qnum[1]) * r**2 * np.sin(theta)
+    sph = sph_harm_R(phi, theta, nlm[1][0], nlm[1][1]) * r**2 * np.sin(theta)
     return coef * fx * fy * fz * radial * sph
 
-def a_int_I(r, theta, phi, qnum, cut_off, mu):
-    """Return real part of integrand to use scipy.integrate.tplquad
+def a_int_I(r, theta, phi, nlm, cut_off, mu):
+    """Return imaginary part of integrand to use scipy.integrate.tplquad
 
     Args:
         r (float): radius
         theta (float): polar angle
         phi (float): azimuthal angle
-        qnum (tuple): tuple of quantum numbers
+        nlm (tuple): tuple of center and tuple of quantum numbers (l, m)
         cut_off (float): cut_off radius
         mu (list): cartesian coordinates of neighboring atom
 
@@ -118,8 +118,8 @@ def a_int_I(r, theta, phi, qnum, cut_off, mu):
 
     # set a value to sigma
     # sigma = 8.5 * 10**(-3)
-    sigma = 0.1
-    # sigma = 1
+    # sigma = 0.1
+    sigma = 1
     # calculate cartesian coordinates from polar coordinates
     x = r * np.cos(phi) * np.sin(theta)
     y = r * np.sin(phi) * np.sin(theta)
@@ -130,40 +130,11 @@ def a_int_I(r, theta, phi, qnum, cut_off, mu):
     fy = gaussian(y, mu[1], sigma)
     fz = gaussian(z, mu[2], sigma)
     # calculate radial function and spherical harmonics
-    gauss = np.exp(- (r-3)**2)
+    gauss = np.exp(- (r-nlm[0])**2)
     func_cut = 0.5 * (np.cos(np.pi * r/cut_off)+1)
     radial = gauss * func_cut
-    sph = sph_harm_I(phi, theta, qnum[0], qnum[1]) * r**2 * np.sin(theta)
+    sph = sph_harm_I(phi, theta, nlm[1][0], nlm[1][1]) * r**2 * np.sin(theta)
     return coef * fx * fy * fz * radial * sph
-
-def calc_order_parameter(vec, quantum, cut_off, params):
-    """Calculate order parameter
-
-    Args:
-        _structure (Structure): crystal structure in POSCAR format
-        each_site (Sites): the position of an atom
-        _neighbors (list): neighboring atoms of each_site
-        quantum (tuple): (l, m)
-        cut_off (float): cut off radius
-        params (float): the central position of gauss function
-
-    Returns:
-        float: order parameter
-    """
-
-    res = 0.0
-    # transform cartesian coordinates into polar coordinates
-    r = np.linalg.norm(vec)
-    theta = np.arccos(vec[2]/r)
-    phi = np.arccos(vec[0]/(r*np.sin(theta)))
-    if vec[1]/np.sin(theta) < 0:
-        phi = -phi + 2 * np.pi
-    # calculate radial function
-    gauss = np.exp(- (r - params)**2)
-    func_cut = 0.5 * (np.cos(np.pi * r/cut_off)+1)
-    radial = gauss * func_cut
-    res += radial * calc_sph_harm(quantum, phi, theta)
-    return res.conjugate()
 
 def calc_sph_harm(sph_indices, phi, theta):
     """Calculate spherical harmonics under given arguments
@@ -182,17 +153,19 @@ def calc_sph_harm(sph_indices, phi, theta):
 
     return sp.sph_harm(sph_indices[1], sph_indices[0], phi, theta)
 
-def calc_opl(vec, ref_dict, cut_off, nmax):
-    """Calculate multi list of order parameters in using spherical harmonics as basis functions
+def calc_order_parameter(vec, ref_dict, cut_off, nmax):
+    """Return ndarray of order parameters in using spherical harmonics as basis functions
 
     Args:
         vec (float): the distance between a site and neighboring atoms
         ref_dict (dict): receive pair of quantum number (l, m) and return index(int)
         cut_off (float): cut off radius
-        nmax (dict): maximum of center position in gauss function
+        nmax (dict): maximum of center position in gaussian function
 
     Returns:
         ndarray: basis function values at the position of a neighboring atom
+
+    In this case, neighboring atomic density is formulated by using delta function.
     """
 
     # prepare data structure
@@ -213,38 +186,31 @@ def calc_opl(vec, ref_dict, cut_off, nmax):
         res[nlm[0], ref_dict[nlm[1]]] = radial * calc_sph_harm(nlm[1], phi, theta)
     return res.conjugate()
 
-def calc_order_parameter2(poscar, ref_dict, cut_off):
+def calc_order_parameter2(vec, ref_dict, cut_off, nmax):
     """Calculate order parameter when taking approximation of rho(i)
 
     Args:
-        poscar (poscar): the path of POSCAR file
-        ref_dict (dict): hash dictionary of l and m
+        vec (list): the distance between a site and neighboring atoms
+        ref_dict (dict): receive pair of quantum number (l, m) and return index(int)
         cut_off (flaot): cut_off radius
+        nmax (int): maximum of center position in gaussian function
 
     Returns:
         ndarray: ndarray of order_parameters
     """
-    _structure = mg.Structure.from_str(open(poscar).read(), fmt="poscar")
-    atom_sites = _structure.sites
-    for i, each_site in enumerate(atom_sites):
-        _neighbors = _structure.get_neighbors(each_site, cut_off)
-        order_parameters = np.array([])
-        for lm in list(ref_dict):
-            a_R = a_I = 0.0
-            for oposite in _neighbors:
-                vec = oposite.coords - each_site.coords
-                a_R += tplquad(a_int_R, 0, 2 * np.pi, lambda phi: 0, lambda phi: np.pi,
-                               lambda phi, theta: 0, lambda phi, theta: cut_off,
-                               (lm, cut_off, vec))[0]
-                if lm[1] != 0:
-                    a_I += tplquad(a_int_I, 0, 2 * np.pi, lambda phi: 0, lambda phi: np.pi,
-                                   lambda phi, theta: 0, lambda phi, theta: cut_off,
-                                   (lm, cut_off, vec))[0]
-            order_parameters = np.append(order_parameters, complex(a_R, a_I))
-        if i == 0:
-            res = order_parameters
-        else:
-            res = np.vstack([res, order_parameters])
+    # prepare data structure
+    res = np.zeros((nmax+1, len(ref_dict)), dtype=np.complex)
+    seq = itertools.product([i for i in range(nmax+1)], list(ref_dict))
+    for nlm in seq:
+        a_R = a_I = 0.0
+        a_R = tplquad(a_int_R, 0, 2 * np.pi, lambda phi: 0, lambda phi: np.pi,
+                       lambda phi, theta: 0, lambda phi, theta: cut_off,
+                       (nlm, cut_off, vec))[0]
+        if nlm[1][1] != 0:
+            a_I = tplquad(a_int_I, 0, 2 * np.pi, lambda phi: 0, lambda phi: np.pi,
+                         lambda phi, theta: 0, lambda phi, theta: cut_off,
+                         (nlm, cut_off, vec))[0]
+        res[nlm[0], ref_dict[nlm[1]]] = complex(a_R, -a_I)
     return res
 
 if __name__ == "__main__":
@@ -271,14 +237,22 @@ if __name__ == "__main__":
                 res = np.zeros((nmax+1, len(ref_dict)), dtype=np.complex)
                 for oposite in _structure.get_neighbors(each_site, cut_off):
                     vec = oposite.coords - each_site.coords
-                    res += calc_opl(vec, ref_dict, cut_off, nmax)
+                    res += calc_order_parameter(vec, ref_dict, cut_off, nmax)
                 pickle.dump(res, open("results/delta/lmax2/"+path+"_"+str(i+1)+".dump", "wb"))
             print(cnt+1)
     elif mode == 2:
         for cnt, path in enumerate(poscars):
             # calculate cut_off radius
             cut_off = scale * cr_values[path]
-            res = calc_order_parameter2("dataset/"+path, ref_dict, cut_off)
-            pickle.dump(res, open("results/normal_dis/order_parameters/lmax2/"+path+".dump", "wb"))
+            # get information of atomic positions by reading POSCAR file
+            _structure = mg.Structure.from_str(open("dataset/"+path).read(), fmt="poscar")
+            atom_sites = _structure.sites
+            for i, each_site in enumerate(atom_sites):
+                res = np.zeros((nmax+1, len(ref_dict)), dtype=np.complex)
+                for oposite in _structure.get_neighbors(each_site, cut_off):
+                    vec = oposite.coords - each_site.coords
+                    res += calc_order_parameter2(vec, ref_dict, cut_off, nmax)
+                dump_path = "results/normal_dis/order_parameters/lmax2/"+path+"_"+str(i+1)+".dump"
+                pickle.dump(res, open(dump_path, "wb"))
             print(cnt+1)
         pickle.dump(ref_dict, open("results/normal_dis/order_paramters/ref_dict.dump", "wb"))
