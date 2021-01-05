124fc748795018d3ed238ad8c48b821413b8a38e
Taiki Iwamura <takki.0206@gmail.com>
2021-01-05 15:01:55 +0900

    Change the parameters of graph drawing

--- graphs/gauss_comparison.py
+++ graphs/gauss_comparison.py
@@ -14,21 +14,16 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     fig, ax = plt.subplots()
-    t = np.linspace(0, 4, 1000)
+    t = np.linspace(0, 4.5, 1000)
     cut_off = 0.5 * (np.cos(np.pi * t/4.5) + 1)
-    y1 = np.exp(-(t - 2.5)**2) * cut_off
-    y2 = np.exp(-(t - 1)**2) * cut_off
-    y3 = np.exp(-(t - 2)**2) * cut_off
-    y4 = np.exp(-(t - 3)**2) * cut_off
-    y5 = np.exp(-(t - 4)**2) * cut_off
-    y6 = np.exp(-(t - 5)**2) * cut_off
-    c1, c2, c3, c4, c5, c6 = "blue", "green", "red", "black", "pink", "purple"
-    ax.set(xlim=(0, 4), xlabel="x")
-    ax.grid()
+    y1 = np.exp(-(t - 1)**2) * cut_off
+    y2 = np.exp(-(t - 2)**2) * cut_off
+    y3 = np.exp(-(t - 3)**2) * cut_off
+    y4 = np.exp(-(t - 4)**2) * cut_off
+    c1, c2, c3, c4 = "blue", "green", "red", "m"
+    ax.set(xlim=(0, 5), ylim=(0, 1.0))
     ax.plot(t, y1, color=c1)
     ax.plot(t, y2, color=c2)
     ax.plot(t, y3, color=c3)
     ax.plot(t, y4, color=c4)
-    ax.plot(t, y5, color=c5)
-    ax.plot(t, y6, color=c6)
     plt.savefig("results/graphs/gcomp.png")
