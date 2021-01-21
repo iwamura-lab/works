e31db611ba27de1a8a5a0d3de951c844a031c07a
Taiki Iwamura <taiki-engineering@outlook.jp>
2021-01-21 09:33:59 +0900

    Add the program to draw graph

---
+++ graphs/lennard_jones.py
@@ -0,0 +1,21 @@
+"""
+Program to draw and save the graph of Lennard Jones potential
+"""
+
+# set python interpreter(2 or 3 ?)
+# !/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+# import module to handle array
+import numpy as np
+
+# import module to draw graph
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    ax = plt.axes()
+    r = np.linspace(0.1, 2, 950)
+    cut_off = 0.5 * (np.cos(np.pi * r/4.5) + 1)
+    ax.plot(r, 1/r**12 - 2/r**6)
+    ax.set(xlim=(0.6, 2.0), ylim=(-1.5, 2.0))
+    plt.savefig("results/graphs/lennardJones.png")
