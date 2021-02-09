43060e94efb8928ff5592cfba6f730c147cc1c5a
server(iwamura) <taiki-engineering@outlook.jp>
2021-02-09 11:39:34 +0900

    finish program making

--- datacheck/count_atoms2.py
+++ datacheck/count_atoms2.py
@@ -5,6 +5,7 @@ Program to count atoms in POSCAR files
 
 # import standard modules
 import os
+import tqdm
 import numpy as np
 
 # import modules made by Seko sensei
@@ -12,7 +13,9 @@ from mlptools.common.readvasp import Poscar
 
 
 if __name__ == "__main__" :
-    paths = os.listdir("/home/iwamura/mlp-Fe/3-dft/init/")
-    atom_numbers = np.array([sum(Poscar(poscarfile).n_atoms) for poscarfile in paths])
+    poscar_dir = "/home/iwamura/mlp-Fe/3-dft/init/"
+    paths = os.listdir(poscar_dir)
+    atom_numbers = np.array([sum(Poscar(poscar_dir+poscarfile).n_atoms)\
+                            for poscarfile in tqdm.tqdm(paths)])
     if np.any(atom_numbers != 32):
         print("There is at least one file without 32 atoms.")
