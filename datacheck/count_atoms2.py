db90f7ee9f96f2980623bdd1c2409def47f16334
server(iwamura) <taiki-engineering@outlook.jp>
2021-02-09 11:21:17 +0900

    make program readable

--- datacheck/count_atoms2.py
+++ datacheck/count_atoms2.py
@@ -1,33 +1,18 @@
+#!/home/iwamura/mlp-Fe/my_works/venv/bin/python3
 """
 Program to count atoms in POSCAR files
 """
 
 # import standard modules
-import argparse
+import os
+import numpy as np
 
 # import modules made by Seko sensei
 from mlptools.common.readvasp import Poscar
 
-def extract_atoms(path):
-    """Extract info of atom counts from POSCAR
-
-    Args:
-        path (str): path to POSCAR
-
-    Returns:
-        list: atom counts
-    """
-    f = open("/home/iwamura/mlp-Fe/3-dft/init/"+path)
-    _ = [f.readline() for i in range(6)]
-    atoms = [int(n) for n in f.readline().split()]
-    f.close()
-    return sum(atoms)
 
 if __name__ == "__main__" :
-    parser = argparse.ArgumentParser()
-    parser.add_argument("path", type=str, help="File path to count atoms. e.g.) poscar-03701.")
-    args = parser.parse_args()
-    st = Poscar(args.path).get_structure()
-    atoms = sum(st[2])
-    if atoms != 32 :
-        print(args.path)
+    paths = os.listdir("/home/iwamura/mlp-Fe/3-dft/init/")
+    atom_numbers = np.array([sum(Poscar(poscarfile).n_atoms) for poscarfile in paths])
+    if np.any(atom_numbers != 32):
+        print("There is at least one file without 32 atoms.")
