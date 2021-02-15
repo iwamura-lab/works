a7fd9f0c786923ac8ddb2561f167d5ed756308fa
server(iwamura) <taiki-engineering@outlook.jp>
2021-02-15 14:54:53 +0900

    count_atoms(completed)

--- datacheck/count_atoms.py
+++ datacheck/count_atoms.py
@@ -1,34 +1,23 @@
+#!/home/iwamura/mlp-Fe/venv/bin/python
 """
 Program to count atoms in POSCAR files
 """
 
 # import standard modules
-import argparse
+import os
+import tqdm
 import numpy as np
 
-def extract_atoms(path):
-    """Extract info of atom counts from POSCAR
+# import modules made by Seko sensei
+from mlptools.common.readvasp import Poscar
 
-    Args:
-        path (str): path to POSCAR
-
-    Returns:
-        list: atom counts
-    """
-    f = open("/home/iwamura/mlp-Fe/3-dft/init/"+path)
-    _ = [f.readline() for i in range(6)]
-    atoms = [int(n) for n in f.readline().split()]
-    f.close()
-    return sum(atoms)
 
 if __name__ == "__main__" :
-    parser = argparse.ArgumentParser()
-    parser.add_argument("paths", type=str, help="File paths to count atoms. e.g.) 1-5000.")
-    args = parser.parse_args()
-    start, end = args.paths.split("-")
-    atom_counts_dict = [extract_atoms("poscar-"+"{:0>5}".format(file_number))
-                        for file_number in range(int(start), int(end)+1)]
-    if np.any(atom_counts_dict != 32):
-        print("There is at least one POSCAR without 32 atoms.")
-    else :
-        print("All POSCAR files have 32 atoms.")
+    poscar_dir = "/home/iwamura/mlp-Fe/3-dft/init/"
+    paths = os.listdir(poscar_dir)
+    atom_numbers = np.array([sum(Poscar(poscar_dir+poscarfile).n_atoms)\
+                            for poscarfile in tqdm.tqdm(paths)])
+    if np.any(atom_numbers != 32):
+        print("There is at least one file without 32 atoms.")
+    else:
+        print("All the POSCAR files have 32 atoms.")
