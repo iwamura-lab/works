3be193c5db676addab04f88d4b68a23b1b88ac28
server(iwamura) <taiki-engineering@outlook.jp>
2021-02-16 16:26:35 +0900

    example 1

--- mlpgen/regression.py
+++ mlpgen/regression.py
@@ -8,7 +8,7 @@ import time
 
 # import standard modules
 import argparse
-import tqdm
+#import tqdm
 import numpy as np
 
 # from mlptools import some modules
@@ -19,7 +19,7 @@ from mlptools.mlpgen.io import ReadFeatureParams
 class TrainStructure:
     def __init__(self, fnames:str, with_force, weight):
         self.vasprun_array = [Vasprun(vasp_path) for ref_file in fnames \
-                              for vasp_path in tqdm.tqdm(np.loadtxt(ref_file, dtype=str)[:, 1])]
+                              for vasp_path in np.loadtxt(ref_file, dtype=str)[:, 1]]
         self.e_array = [v.get_energy() for v in self.vasprun_array]
         self.f_array = [np.ravel(v.get_forces(), order='F') for v in self.vasprun_array]
         self.axis_array = [v.get_structure()[0] for v in self.vasprun_array]
@@ -28,9 +28,16 @@ class TrainStructure:
         self.vol_array = [v.get_structure()[3] for v in self.vasprun_array]
         self.elm_array = [v.get_structure()[4] for v in self.vasprun_array]
         self.type_array = [v.get_structure()[5] for v in self.vasprun_array]
-        s_array = [self.extract_s(v.get_stress)]
 
-    def extract_s(self, s:StressTensor):
+    def extract_s(self, s):
+        """Extract xx, yy, zz, xy, yz, zx components from Stress Tensor.
+
+        Args:
+            s (multi_list): Stress Tensor
+
+        Returns:
+            list: xx, yy, zz, xy, yz, zx in order
+        """
         return [s[0][0], s[1][1], s[2][2], s[0][1], s[1][2], s[2][0]]
 
 
