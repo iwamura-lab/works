40739da22a07540eba193e8aa199c198f090ee2a
server(iwamura) <taiki-engineering@outlook.jp>
2021-02-19 19:46:52 +0900

    Revise verbose part of clebsch_gordan

--- projmat/invariants.py
+++ projmat/invariants.py
@@ -10,22 +10,20 @@ in order to use other functions as basis functions in the near future
 from itertools import product
 import sys
 from math import factorial, sqrt
-
-# import modules to operate matrix
 import numpy as np
 
 # import modules to use mathematical functions
-# from sympy.physics.wigner import clebsch_gordan as my_clebsch
+# from sympy.physics.wigner import clebsch_gordan as clebsch_gordan
 
-def my_clebsch(j1, j2, j3, m1, m2, m3):
-    """Calculate clebsch gordan coefficient from input arguments
+def clebsch_gordan(j1, j2, j3, m1, m2, m3):
+    """Calculate clebsch gordan coefficient from the quantum numbers
 
     Args:
-        j1 (int): angular momentum of eigenstate
-        j2 (int): angular momentum of eigenstate
+        j1 (int): angular momentum of 1st eigenstate before coupling
+        j2 (int): angular momentum of 2nd eigenstate before coupling
         j3 (int): angular momentum after coupling
-        m1 (int): projection angular momentum of eigenstate
-        m2 (int): projection angular momentum of eigenstate
+        m1 (int): projection angular momentum of 1st eigenstate before coupling
+        m2 (int): projection angular momentum of 2nd eigenstate before coupling
         m3 (int): projection angular momentum after coupling
 
     Returns:
@@ -33,34 +31,34 @@ def my_clebsch(j1, j2, j3, m1, m2, m3):
 
     Sympy is the module to use symbolic algebra, so its behavior is too slow
     when using high order matrix.
-    When only the result value is needed, this function is useful.
+    When only the output value is needed, this function is useful.
     """
-    # kronecker's delta item
+    # judge whether clebsch gordan coefficient is zero or not
     if m1 + m2 != m3:
         return 0
-    # determine the range of v
+    denominator = [j3+j1-j2, j3-j1+j2, j1+j2-j3, j3+m3, j3-m3]
+    numerator = [j1+j2+j3+1, j1-m1, j1+m1, j2-m2, j2+m2]
+    # It is obvious that "j1+j2-j3", "j3+m3" and "j3-m3" >= 0
+    for i in denominator[:2]:
+        if i < 0:
+            return 0
+    # It is obvious that "j1+j2+j3+1", "j1-m1", "j1+m1", "j2-m2" and "j2+m2" >= 0
+
+    # determine the range of sum index
     max1 = j2 + j3 + m1
-    max2 = j3 - j1 + j2
-    max3 = j3 + m3
-    min1 = m1 - j1
+    max2 = j3 - j1 + j2 # in denominator
+    max3 = j3 + m3 # >=0
+    min1 = m1 - j1 # <=0
     min2 = j2 - j1 + m3
     vmax = min(max1, max2, max3)
     vmin = max(min1, min2, 0)
-    # make factorial accept negative value
-    deno = [j3+j1-j2, j3-j1+j2, j1+j2-j3, j3+m3, j3-m3]
-    nume = [j1+j2+j3+1, j1-m1, j1+m1, j2-m2, j2+m2]
-    for i in deno:
-        if i < 0:
-            return 0
-    for i in nume:
-        if i < 0:
-            return 0
-    dfac = 1
-    for i in deno:
-        dfac *= factorial(i)
-    nfac = 1
-    for i in nume:
-        nfac *= factorial(i)
+    # make
+    dfactorial = 1
+    for i in denominator:
+        dfactorial *= factorial(i)
+    nfactorial = 1
+    for i in numerator:
+        nfactorial *= factorial(i)
     cf = sqrt((2*j3+1)*dfac / nfac)
     csum = 0
     for v in range(vmin, vmax+1):
@@ -124,8 +122,8 @@ if __name__ == "__main__":
         mid = {c: i for i, c in enumerate(id_list)}
         for cm in id_list:
             for rm in id_list:
-                c1 = my_clebsch(lis[0], lis[1], lis[2], cm[0], cm[1], -cm[2])
-                c2 = my_clebsch(lis[0], lis[1], lis[2], rm[0], rm[1], -rm[2])
+                c1 = clebsch_gordan(lis[0], lis[1], lis[2], cm[0], cm[1], -cm[2])
+                c2 = clebsch_gordan(lis[0], lis[1], lis[2], rm[0], rm[1], -rm[2])
                 pmat[mid[cm], mid[rm]] = (-1)**(cm[2]-rm[2])/(2*lis[2]+1)*c1*c2
     if len(lis) == 4:
         id_list = list(product(range(-lis[0], lis[0]+1), range(-lis[1], lis[1]+1),
@@ -135,10 +133,10 @@ if __name__ == "__main__":
             for rm in id_list:
                 p = 0
                 for l in range(abs(lis[0]-lis[1]), lis[0]+lis[1]):
-                    c1 = my_clebsch(lis[0], lis[1], l, cm[0], cm[1], cm[0]+cm[1])
-                    c2 = my_clebsch(lis[0], lis[1], l, rm[0], rm[1], rm[0]+rm[1])
-                    c3 = my_clebsch(lis[2], l, lis[3], cm[2], cm[0]+cm[1], -cm[3])
-                    c4 = my_clebsch(lis[2], l, lis[3], rm[2], rm[0]+rm[1], -rm[3])
+                    c1 = clebsch_gordan(lis[0], lis[1], l, cm[0], cm[1], cm[0]+cm[1])
+                    c2 = clebsch_gordan(lis[0], lis[1], l, rm[0], rm[1], rm[0]+rm[1])
+                    c3 = clebsch_gordan(lis[2], l, lis[3], cm[2], cm[0]+cm[1], -cm[3])
+                    c4 = clebsch_gordan(lis[2], l, lis[3], rm[2], rm[0]+rm[1], -rm[3])
                     p += (-1)**(rm[3]-cm[3])/(2*lis[3]+1)*c1*c2*c3*c4
                 pmat[mid[cm], mid[rm]] = p
     if len(lis) == 5:
@@ -151,12 +149,12 @@ if __name__ == "__main__":
                 p = 0
                 for l in range(abs(lis[0]-lis[1]), lis[0]+lis[1]):
                     for L in range(abs(lis[2]-l), lis[2]+l):
-                        c1 = my_clebsch(lis[0], lis[1], l, cm[0], cm[1], cm[0]+cm[1])
-                        c2 = my_clebsch(lis[0], lis[1], l, rm[0], rm[1], rm[0]+rm[1])
-                        c3 = my_clebsch(lis[2], l, L, cm[2], cm[0]+cm[1], cm[0]+cm[1]+cm[2])
-                        c4 = my_clebsch(lis[2], l, L, rm[2], rm[0]+rm[1], rm[0]+rm[1]+rm[2])
-                        c5 = my_clebsch(lis[3], L, lis[4], cm[3], cm[0]+cm[1]+cm[2], -cm[4])
-                        c6 = my_clebsch(lis[3], L, lis[4], rm[3], rm[0]+rm[1]+rm[2], -rm[4])
+                        c1 = clebsch_gordan(lis[0], lis[1], l, cm[0], cm[1], cm[0]+cm[1])
+                        c2 = clebsch_gordan(lis[0], lis[1], l, rm[0], rm[1], rm[0]+rm[1])
+                        c3 = clebsch_gordan(lis[2], l, L, cm[2], cm[0]+cm[1], cm[0]+cm[1]+cm[2])
+                        c4 = clebsch_gordan(lis[2], l, L, rm[2], rm[0]+rm[1], rm[0]+rm[1]+rm[2])
+                        c5 = clebsch_gordan(lis[3], L, lis[4], cm[3], cm[0]+cm[1]+cm[2], -cm[4])
+                        c6 = clebsch_gordan(lis[3], L, lis[4], rm[3], rm[0]+rm[1]+rm[2], -rm[4])
                         p += (-1)**(cm[4]-rm[4])*1/(2*lis[4]+1)*c1*c2*c3*c4*c5*c6
                 pmat[mid[cm], mid[rm]] = p
     if len(lis) == 6:
@@ -170,17 +168,17 @@ if __name__ == "__main__":
                 for l in range(abs(lis[0]-lis[1]), lis[0]+lis[1]):
                     for L in range(abs(lis[2]-l), lis[2]+l):
                         for S in range(abs(lis[3]-L), lis[3]+L):
-                            c1 = my_clebsch(lis[0], lis[1], l, cm[0], cm[1], cm[0]+cm[1])
-                            c2 = my_clebsch(lis[0], lis[1], l, rm[0], rm[1], rm[0]+rm[1])
-                            c3 = my_clebsch(lis[2], l, L, cm[2], cm[0]+cm[1], cm[0]+cm[1]+cm[2])
-                            c4 = my_clebsch(lis[2], l, L, rm[2], rm[0]+rm[1], rm[0]+rm[1]+rm[2])
-                            c5 = my_clebsch(lis[3], L, S, cm[3], cm[0]+cm[1]+cm[2],
+                            c1 = clebsch_gordan(lis[0], lis[1], l, cm[0], cm[1], cm[0]+cm[1])
+                            c2 = clebsch_gordan(lis[0], lis[1], l, rm[0], rm[1], rm[0]+rm[1])
+                            c3 = clebsch_gordan(lis[2], l, L, cm[2], cm[0]+cm[1], cm[0]+cm[1]+cm[2])
+                            c4 = clebsch_gordan(lis[2], l, L, rm[2], rm[0]+rm[1], rm[0]+rm[1]+rm[2])
+                            c5 = clebsch_gordan(lis[3], L, S, cm[3], cm[0]+cm[1]+cm[2],
                                             cm[0]+cm[1]+cm[2]+cm[3])
-                            c6 = my_clebsch(lis[3], L, S, rm[3], rm[0]+rm[1]+rm[2],
+                            c6 = clebsch_gordan(lis[3], L, S, rm[3], rm[0]+rm[1]+rm[2],
                                             rm[0]+rm[1]+rm[2]+rm[3])
-                            c7 = my_clebsch(lis[4], S, lis[5], cm[4],
+                            c7 = clebsch_gordan(lis[4], S, lis[5], cm[4],
                                             cm[0]+cm[1]+cm[2]+cm[3], -cm[5])
-                            c8 = my_clebsch(lis[4], S, lis[5], rm[4],
+                            c8 = clebsch_gordan(lis[4], S, lis[5], rm[4],
                                             rm[0]+rm[1]+rm[2]+rm[3], -rm[5])
                             p += (-1)**(cm[5]-rm[5])*1/(2*lis[5]+1)*c1*c2*c3*c4*c5*c6*c7*c8
                 pmat[mid[cm], mid[rm]] = p
